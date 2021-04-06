bca798e1a084d4b0d3f864f6f417f31dcf05ac6c
Deep Anandrao <8665209+Deepakanandrao@users.noreply.github.com>
2021-04-05 21:08:25 -0400

    Hex to color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import hexToRgba from 'hex-to-rgba';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  ext = 'public static let myColor:Color = Color(UIColor(red:#/255, green:#/255, blue:#/255, alpha:1))';
+  inputCodes = "#112233 #112233 #112233";
+  outputExtension = "";
+  codes = [];
+
+  public generate(event:Event): void{
+    // Read Input and use newline or space char to break it into an array
+    this.inputCodes = this.stringToArray(this.inputCodes);
+    this.codes =  this.inputCodes.split(" ").map((e)=>hexToRgba(e));
+    // Use hexto rbga or map that to a new array 
+    this.outputExtension = "extension Color {\n\t".concat(this.codes.join("\n\t")).concat("\n}");
+    // Generate output
+  }
+
+  public stringToArray(codes:String):any {
+    return codes.replace("\n"," ").replace(","," ").replace("  "," ");
+  }
 }
